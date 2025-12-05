ec81aa22cc75ab547c121faa2427f1d08a495bba
Yichao Wu <wuyichao71@126.com>
2025-12-05 14:08:36 +0900

    add header link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,32 @@
 import NavBar from "./NavBar";
-import "./Header.css";
+import { Link } from "react-router-dom";
 import { base } from "../utility.tsx";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header className="Header-container">
-      <h1 className="Header-title lato-bold">
-        <img src={`${base}title.png`} width={40} height={40}></img>
-        <span style={{ color: "#535860", textShadow: "0px 0px 2px #595959ff" }}>
-          BIO
-        </span>
-        <span style={{ color: "#3E98BF", textShadow: "0px 0px 2px #595959ff" }}>
-          SIM
-        </span>
-      </h1>
+      <Link to={base} className="Header-link">
+        <h1 className="Header-title lato-bold">
+          <img src={`${base}title.png`} width={40} height={40}></img>
+          <span
+            style={{
+              color: "#535860",
+              textShadow: "0px 0px 2px #595959ff",
+            }}
+          >
+            BIO
+          </span>
+          <span
+            style={{
+              color: "#3E98BF",
+              textShadow: "0px 0px 2px #595959ff",
+            }}
+          >
+            SIM
+          </span>
+        </h1>
+      </Link>
       <NavBar />
     </header>
   );
